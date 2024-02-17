e6ce16bf2f4abd45a7ef021765302539750a6844
zxcv0028 <bad020906@gmail.com>
2024-02-17 22:25:58 +0900

    [feat] create inventory

--- Igloo/Assets/Code/Scripts/Inventory/InventoryManager.cs
+++ Igloo/Assets/Code/Scripts/Inventory/InventoryManager.cs
@@ -9,7 +9,9 @@ public class InventoryManager : MonoBehaviour
     public List<Item> Items = new List<Item>();
 
     public Transform ItemContent;
-    public GameObject InventoryItem;
+    public GameObject InventoryItemPrefab;
+
+    private Dictionary<Item, GameObject> itemUIObjects = new Dictionary<Item, GameObject>();
 
     private void Awake()
     {
@@ -19,23 +21,40 @@ public class InventoryManager : MonoBehaviour
     public void Add(Item item)
     {
         Items.Add(item);
+
+        if (!itemUIObjects.ContainsKey(item))
+        {
+            GameObject obj = Instantiate(InventoryItemPrefab, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+
+            itemUIObjects.Add(item, obj);
+        }
     }
 
     public void Remove(Item item)
     {
         Items.Remove(item);
+
+        if (itemUIObjects.ContainsKey(item))
+        {
+            Destroy(itemUIObjects[item]);
+            itemUIObjects.Remove(item);
+        }
     }
 
     public void ListItems()
     {
         foreach (var item in Items)
         {
-            GameObject obj = Instantiate(InventoryItem, ItemContent);
-            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
-            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
-
-            itemName.text = item.itemName;
-            itemIcon.sprite = item.icon;
+            if (itemUIObjects.ContainsKey(item))
+            {
+                GameObject obj = itemUIObjects[item];
+                obj.SetActive(true);
+            }
         }
     }
 }
