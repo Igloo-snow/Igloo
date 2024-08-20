5d2127145130ea4a5e12ab08ebc03eb02322d27b
mynzi1 <mynzi00@sookmyung.ac.kr>
2024-08-20 15:02:06 +0900

    [feat] add PLT map
    - add PLT map
    - add tree's roots

--- Igloo/Assets/Code/Scripts/Dungeon/PLT/PLTLever.cs
+++ Igloo/Assets/Code/Scripts/Dungeon/PLT/PLTLever.cs
@@ -12,6 +12,16 @@ public class PLTLever : Interactable
 
     public GameObject dragDropTest;
     public GameObject problemUI;
+    public GameObject glass;
+    public GameObject playerModel;
+
+    public GameObject pot;
+    public GameObject root;
+
+    public AudioClip right;
+    public AudioClip wrong;
+
+
 
     void Start()
     {
@@ -19,6 +29,9 @@ public class PLTLever : Interactable
         isMissionOn = false;
         dragDropTest.SetActive(false);
         problemUI.SetActive(false);
+
+        pot.SetActive(true);
+        root.SetActive(false);
 
         targetCameras[0].enabled = false;
         targetCameras[1].enabled = false;
@@ -39,14 +52,22 @@ public class PLTLever : Interactable
             targetCameras[1].enabled = false;
             dragDropTest.SetActive(false);
             problemUI.SetActive(false);
+
+            glass.SetActive(true);
+            playerModel.SetActive(true);
+
+            pot.SetActive(true);
+            root.SetActive(false);
         }
         else
         {
             targetCameras[0].enabled = true;
             playerMovement.enabled = false;
 
+            playerModel.SetActive(false);
+
             Invoke("ActivateDragDrop", 2f);
-            Invoke("ActivateObjs", 4.5f);
+            Invoke("SwitchActivateObjs", 4.5f);
         }
         isMissionOn = !isMissionOn;
     }
@@ -54,11 +75,22 @@ public class PLTLever : Interactable
     private void ActivateDragDrop()
     {
         targetCameras[1].enabled = true;
+        glass.SetActive(false);
+
     }
 
-    private void ActivateObjs()
+    private void SwitchActivateObjs()
     {
         dragDropTest.SetActive(true);
         problemUI.SetActive(true);
+
+        pot.SetActive(false);
+        root.SetActive(true);
+
+    }
+
+    public void RightAnswer()
+    {
+        SoundManager.instance.Play(right);
     }
 }
